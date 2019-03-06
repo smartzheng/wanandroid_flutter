8ca0745390fdfc54de6d81cc26b520636fe1f7d4
shulin <shulin@uoko.com>
2019-03-06 18:04:49 +0800

    完成主体结构

---
+++ lib/pages/discover/DiscoverPage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DiscoverPage extends StatefulWidget {
+  DiscoverPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _DiscoverPageState createState() => _DiscoverPageState();
+}
+
+class _DiscoverPageState extends State<DiscoverPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('发现'),
+    );
+  }
+}
